82cf2282af10d56f00cca3d4f82234a1e19b7e90
nurbekovv05 <baevnurbol@gmail.com>
2023-05-29 18:46:15 +0600

    fs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import HeadLineHard from "./components/HeadLineHard";
+import Food from "./components/Food";
 
 function App() {
     return (
         <div className="App">
             <Navbar/>
             <Hero/>
+            <HeadLineHard/>
+            <Food/>
         </div>
     );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <div className='max-w-[1640px] mx-auto p-4'>
             <div className='max-h-[500px] relative'>
-                <div className='absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center'>
+                <div className='absolute w-full h-full  max-h-[500px] bg-black/40 flex flex-col justify-center mt-[210px]'>
                     <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span className='text-orange-500'>Best</span></h1>
                     <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>Foods <span className='text-orange-500'>Delivired</span></h1>
                 </div>
